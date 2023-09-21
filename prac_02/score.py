5059b834a5d7ad58aa24afdb9d14211c92373650
ShaneLind <shane.lind@my.jcu.edu.au>
2023-09-21 14:27:42 +1000

    Add score classifier using functions, program operates using a while choice loop

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,30 +2,32 @@
 CP1404 - Practical
 Broken program to determine score status
 """
+import random
 
-###########################
-# Broken example code
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#     print("Excellent")
-# if score < 50:
-#     print("Bad")
 
-####################################
-# Working efficient code
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get user input as score and prints the score's classification"""
+    score = float(input("Enter score: "))
+    result = classify_the_score(score)
+    print(f"user score is {result}")
+
+    random_score = random.randint(0, 100)
+    print(f"Random score is: {random_score}")
+    result = classify_the_score(random_score)
+    print(f"random score is {result}")
+
+
+def classify_the_score(score):
+    """Classify score"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
