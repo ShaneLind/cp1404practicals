6a70657f905ea7710d00935c3fccafba74f48bdd
ShaneLind <shane.lind@my.jcu.edu.au>
2023-09-21 14:01:54 +1000

    Add password checker using functions, program operates using a while choice loop

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,14 +2,43 @@
 Shane Lind
 Password check program
 """
-min_length = 3
-password = input("Enter a password: ")
 
-while True:
-    length = len(password)
-    if length >= min_length:
-        print('*' * length)
-        break
-    else:
-        print("Invalid word length (too short)")
+
+def main():
+    """Get user choice to record password or print password length"""
+    MENU = """G - Get Password
+P - Print Password Length in Asterisks
+Q - Quit"""
+    print(MENU)
+    password = ""
+    min_length = 3
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            password = get_password(min_length)
+            print(MENU)
+        elif choice == "P":
+            print_asterisks(password)
+            print(MENU)
+        else:
+            print("Invalid Name")
+            password = get_password(min_length)
+        choice = input(">>> ").upper()
+        print(MENU)
+
+
+def get_password(min_length):
+    """ Get user input as password and check if it is of valid length"""
+    password = input("Enter a password: ")
+    while len(password) < min_length:
+        print("Invalid Name (too short)")
         password = input("Enter a password: ")
+    return password
+
+
+def print_asterisks(password):
+    """Print the length of password as asterisks"""
+    print('*' * len(password))
+
+
+main()
