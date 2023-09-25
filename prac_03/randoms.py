393177130baaae38ee2d4f4ae2f129712d692b29
ShaneLind <shane.lind@my.jcu.edu.au>
2023-09-25 15:08:27 +1000

    Add Randoms file

---
+++ prac_03/randoms.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical
+Randoms
+"""
+
+import random
+
+# print(random.randint(5, 20))  # line 1
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# Answer: The smallest number possible was 5, the largest number was 20
+
+# print(random.randrange(3, 10, 2))  # line 2
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# Answer:The smallest number possible was 3, the largest number was 9, line 2 cannot produce a 4
+
+# print(random.uniform(2.5, 5.5))  # line 3
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# Answer: The smallest number possible was 2.5, the largest number was 5.5
+
+# produce a random number between 1 and 100 inclusive
+print(random.randrange(1, 100))
