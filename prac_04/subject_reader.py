586f3bb85bb4de09529437e526986412d3e8e3d6
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-05 11:29:33 +1000

    Add read data from file and will display list and subject data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,35 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Read data form list and display subject details"""
     data = get_data()
     print(data)
+    get_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    nested_list = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        nested_list += [parts]
     input_file.close()
+    return nested_list
+
+
+def get_details(data):
+    """Decode list and print subject details"""
+    for detail in data:
+        subject_details = detail
+        print(f"{subject_details[0]} is taught by {subject_details[1]} and has {subject_details[2]} students")
 
 
 main()
