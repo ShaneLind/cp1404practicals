3c412e437162d2a464e7fe9bcb26b23b16432e6f
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-09 16:54:48 +1000

    Add case insensitive hexadecimal colour codes in a dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,14 +2,19 @@
 CP1404Practical
 hexadecimal colour codes in a dictionary
 """
+from requests.structures import CaseInsensitiveDict
 
-NAME_TO_CODE = {"Absolute Zero": "#0048ba"}
-print(NAME_TO_CODE)
+colour_to_code = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
+                "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                "Aqua": "#00ffff", "Aureolin": "#fdee00", "Brilliant Rose": "#ff55a3"}
+name_to_code = CaseInsensitiveDict(colour_to_code)
+
+print(name_to_code)
 
 colour_code = input("Enter colour name: ")
 while colour_code != "":
     try:
-        print(colour_code, "is", NAME_TO_CODE[colour_code])
+        print(colour_code, "is", name_to_code[colour_code])
     except KeyError:
         print("Invalid colour name")
     colour_code = input("Enter short state: ")
