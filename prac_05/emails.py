58a309c86974ab99511abfacec4344fbd7189e8a
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-10 14:42:04 +1000

    add dictionary storing emails and names

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,20 +1,32 @@
 """
 Emails
 Estimate: 30 minutes
-Actual: ?? minutes
+Actual: 50 minutes
 """
 
 email = str(input("Email: "))
-name = email.split('.')
-first_name = name[0]
-last_name = name[1].split('@')
-while email != "":
-    # print(email)
-    print(f"Is your name {first_name} {last_name[0]}?")
-    answer = input("(Y/N): ").lower()
-    if answer == 'y' or answer == "":
-        print(f"Name: {first_name} {last_name[0]}")
-    elif answer == 'n':
-        email = str(input("Email: "))
+email_to_name = {}
 
-    email = str(input("Email: "))
+while email != "":
+    # decipher user's name
+    sections = email.split('@')
+    if '.' in sections[0]:
+        name = ' '.join(sections[0].split('.'))
+    else:
+        name = str(sections[0])
+
+    # check if user's name is correct
+    print(f"Is your name {name}?")
+    answer = input("(Y/N): ").lower()
+    if answer == 'y' or answer == "":  # if name is correct
+        print(f"Name: {name}")  # return user's name
+    elif answer == 'n':  # if name is correct
+        name = str(input("Name: "))  # get user to input correct name
+    email_to_name[email] = name  # update dictionary
+    email = str(input("Email: "))  # ask for next input
+
+# print dictionary
+keys = list(email_to_name.keys())
+max_length = max(len(key) for key in keys)
+for email, name in email_to_name.items():
+    print(f"Email: {email:{max_length + 1}}- Name: {name:1}")
