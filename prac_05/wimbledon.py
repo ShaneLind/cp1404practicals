cc1ff5c731361166c60149af30198c219d15b84f
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-10 16:54:42 +1000

    add updated functions for better functionality

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,22 +8,17 @@ Actual: 70 minutes
 def main():
     """Get and print the number of wins per champion and the countries of champions in alphabetical order"""
     filename = "wimbledon.csv"
-    countries = []
-    names = []
 
-    # open file
-    with open(filename, encoding="utf-8-sig") as in_file:
-        countries, names = get_champion_information(countries, in_file, names)
+    countries, names = get_champion_information(filename)
+    champion_to_wins = get_number_of_wins_per_champion(names)
+    countries_alphabetical = get_countries_in_alphabetical_order(countries)
 
-        champion_to_wins = get_number_of_wins_per_champion(names)
-        countries_alphabetical = get_countries_in_alphabetical_order(countries)
-
-        # print results
-        max_length = get_max_length_for_printing(champion_to_wins)
-        print("Wimbledon Champions:")
-        for name, wins in champion_to_wins.items():
-            print(f"{name:{max_length + 1}}- Wins: {wins:1}")
-        print(f"These 12 countries have won Wimbledon:\n {countries_alphabetical}")
+    # print results
+    max_length = get_max_length_for_printing(champion_to_wins)
+    print("Wimbledon Champions:")
+    for name, wins in champion_to_wins.items():
+        print(f"{name:{max_length + 1}}- Wins: {wins:1}")
+    print(f"These 12 countries have won Wimbledon:\n {countries_alphabetical}")
 
 
 def get_max_length_for_printing(champion_to_wins):
@@ -44,13 +39,17 @@ def get_number_of_wins_per_champion(names):
     return champion_to_wins
 
 
-def get_champion_information(countries, in_file, names):
+def get_champion_information(filename):
     """Collect champion names and countries"""
-    in_file.readline()  # ignore header
-    for line in in_file:
-        parts = line.strip().split(',')
-        countries += [parts[1]]
-        names += [parts[2]]
+    countries = []
+    names = []
+    # open file
+    with open(filename, encoding="utf-8-sig") as in_file:
+        in_file.readline()  # ignore header
+        for line in in_file:
+            parts = line.strip().split(',')
+            countries += [parts[1]]
+            names += [parts[2]]
     in_file.close()  # close file
     return countries, names
 
