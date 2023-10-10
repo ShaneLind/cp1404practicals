3767468834153138e91ca0cf5ba287b5d73d1a74
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-10 13:22:50 +1000

    Add decypher name form email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,9 +4,14 @@ Estimate: 30 minutes
 Actual: ?? minutes
 """
 
-email = str(input("Text: "))
+email = str(input("Email: "))
+name = email.split('.')
+first_name = name[0]
+last_name = name[1].split('@')
 while email != "":
     print(email)
+    print(first_name, last_name[0])
+
     email = str(input("Email: "))
 
 
