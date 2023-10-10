8543fdff16246ed078f8a0995d1b2bb60eb91175
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-10 13:29:22 +1000

    Add Y/N option to confirm email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,9 +9,12 @@ name = email.split('.')
 first_name = name[0]
 last_name = name[1].split('@')
 while email != "":
-    print(email)
-    print(first_name, last_name[0])
+    # print(email)
+    print(f"Is your name {first_name} {last_name[0]}?")
+    answer = input("(Y/N): ").lower()
+    if answer == 'y' or answer == "":
+        print(f"Name: {first_name} {last_name[0]}")
+    elif answer == 'n':
+        email = str(input("Email: "))
 
     email = str(input("Email: "))
-
-
