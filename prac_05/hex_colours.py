7267278ef3a90fda14eec875a1dba487f012d59f
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-12 09:29:07 +1000

    add better method of allowing lower case input

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,19 +2,17 @@
 CP1404Practical
 hexadecimal colour codes in a dictionary
 """
-from requests.structures import CaseInsensitiveDict
 
 colour_to_code = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
                 "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
                 "Aqua": "#00ffff", "Aureolin": "#fdee00", "Brilliant Rose": "#ff55a3"}
-name_to_code = CaseInsensitiveDict(colour_to_code)
 
-print(name_to_code)
+print(colour_to_code)
 
-colour_code = input("Enter colour name: ")
+colour_code = input("Enter colour name: ").title()
 while colour_code != "":
     try:
-        print(colour_code, "is", name_to_code[colour_code])
+        print(colour_code, "is", colour_to_code[colour_code])
     except KeyError:
         print("Invalid colour name")
-    colour_code = input("Enter short state: ")
+    colour_code = input("Enter short state: ").title()
