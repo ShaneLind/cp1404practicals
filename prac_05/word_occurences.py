e6934007069e216453609b805683ecb157580f3e
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-13 11:04:57 +1000

    remove commented out code and fix spelling error

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -4,18 +4,14 @@ Estimate: 30 minutes
 Actual: 40 minutes
 """
 
-# get user input and creat dictionary
 text = str(input("Text: "))
 words = text.split(' ')
 word_to_count = {word: words.count(word) for word in words}
-# print(word_to_count)
 
-# sort dictionary
 keys = list(word_to_count.keys())
 keys.sort()
 key_to_count = {key: words.count(key) for key in keys}
 max_length = max(len(key) for key in keys)
 
-# print sorted dictionary
 for key, count in key_to_count.items():
     print(f"{key:{max_length + 1}}: {count:1}")
