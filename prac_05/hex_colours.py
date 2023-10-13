ae8b26921e71b550362ed641b44bfdce03ea5ac0
ShaneLind <shane.lind@my.jcu.edu.au>
2023-10-13 11:02:20 +1000

    add better method of allowing lower case input

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,16 +3,17 @@ CP1404Practical
 hexadecimal colour codes in a dictionary
 """
 
-colour_to_code = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
                 "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
                 "Aqua": "#00ffff", "Aureolin": "#fdee00", "Brilliant Rose": "#ff55a3"}
 
-print(colour_to_code)
+print(COLOUR_TO_CODE)
+COLOUR_TO_CODE = {colour_code.lower(): hex_code for colour_code, hex_code in COLOUR_TO_CODE.items()}
 
-colour_code = input("Enter colour name: ").title()
+colour_code = input("Enter colour name: ").lower()
 while colour_code != "":
     try:
-        print(colour_code, "is", colour_to_code[colour_code])
+        print(colour_code, "is", COLOUR_TO_CODE[colour_code])
     except KeyError:
         print("Invalid colour name")
-    colour_code = input("Enter short state: ").title()
+    colour_code = input("Enter short state: ").lower()
