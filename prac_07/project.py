bef8453613c8cea789adb2435de64c0c96d436bf
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-02 13:04:43 +1000

    Add basic class funcitonality

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,5 +6,13 @@ CP1404 Practical - Project class
 class Project:
     """Project Class """
 
-    def __init__(self):
-        """Initialise fields of Project Class"""
+    def __init__(self, name="", start_date=0, priority=0, cost_estimate=0, completion_percentage=0):
+        """Initialise a project instance"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"({self.name}) , ({self.start_date}) , ({self.priority}) , ({self.cost_estimate}) , ({self.completion_percentage})"
