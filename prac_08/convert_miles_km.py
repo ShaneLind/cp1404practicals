ec7a73750428ac68d365db3baac499eb9fe2ceaf
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-09 14:24:46 +1000

    Add box layout and app building

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Week 8 Practical
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class ConvertMilesKm(App):
+    """ ConvertMilesKm is a Kivy App for converting miles to km """
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Square Number"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+ConvertMilesKm.run()
