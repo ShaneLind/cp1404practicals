838b07632fbaed2b4008dce66c38b929b2ce9d42
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-09 14:41:58 +1000

    Add convert miles to km on button press

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,10 +4,12 @@ CP1404 Week 8 Practical
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class ConvertMilesKm(App):
     """ ConvertMilesKm is a Kivy App for converting miles to km """
+    message = StringProperty()
 
     def build(self):
         """ build the Kivy app from the kv file """
@@ -15,8 +17,17 @@ class ConvertMilesKm(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.message = self.root.ids.user_input.text
+
+    def handle_conversion(self, value):
+        """Handle conversion of miles to km"""
+        try:
+            result = float(value) * 1.609344
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            pass
+
 
 ConvertMilesKm().run()
-
-
-
