156a71de6202a74f051d7beffe2de2f8d141152e
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 13:43:47 +1000

    add rounding the fare price to the nearest 10c

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -20,7 +20,7 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         """Return the price for the taxi trip like parent class,
         but adds an extra charge for each now fare."""
-        current_fare = self.price_per_km * self.current_fare_distance + (self.flagfall * self.number_of_fares)
+        current_fare = super().get_fare() + (self.flagfall * self.number_of_fares)
         self.number_of_fares += 1
         return current_fare
 
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -11,11 +11,11 @@ my_taxi2 = SilverServiceTaxi(name="Vehicle", fuel=200, fanciness=2)
 my_taxi.start_fare()
 my_taxi.drive(0)
 print(my_taxi.__str__())
-print(f"Current fare = ${my_taxi.get_fare()}")
+print(f"Current fare = ${my_taxi.get_fare():.2f}")
 
 my_taxi2.start_fare()
 my_taxi2.drive(18)
 print(my_taxi2.__str__())
-print(f"Current fare = ${my_taxi2.get_fare()}")
+print(f"Current fare = ${my_taxi2.get_fare():.2f}")
 
 
