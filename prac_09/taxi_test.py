50f5bbf2abca3cea27c54520d4b240e35f4aab8f
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 12:47:06 +1000

    Add shared price_per_km variable for all taxi instances

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,7 +4,7 @@ CP1404 Practical
 # from car import Car
 from taxi import Taxi
 
-my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
+my_taxi = Taxi(name="Prius 1", fuel=100)
 
 my_taxi.start_fare()
 my_taxi.drive(40)
