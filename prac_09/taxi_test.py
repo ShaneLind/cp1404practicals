43afc9792abcacfad755a3b2fe51df423a6cd79e
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 12:43:19 +1000

    Add driving the taxi and printing its details and fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,3 +1,17 @@
 """
 CP1404 Practical
 """
+# from car import Car
+from taxi import Taxi
+
+my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
+
+my_taxi.start_fare()
+my_taxi.drive(40)
+print(my_taxi.__str__())
+print(f"Current fare = ${my_taxi.get_fare()}")
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi.__str__())
+print(f"Current fare = ${my_taxi.get_fare()}")
