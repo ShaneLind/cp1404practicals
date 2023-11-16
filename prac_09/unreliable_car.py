60bc364a274a12b2264a3cc11cfc8b4eb6de97ed
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 12:51:04 +1000

    Add unreliable car class and test files

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,9 @@
+"""
+CP1404 Practical
+UnreliableCar Class
+"""
+from car import Car
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability."""
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,5 @@
+"""
+CP1404 Practical
+UnreliableCar test file
+"""
+from unreliable_car import UnreliableCar
