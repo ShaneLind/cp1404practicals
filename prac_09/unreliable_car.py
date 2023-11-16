4e27acf6020ea3a536c04d7e841101540bca3275
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 12:53:28 +1000

    Add initialise instance of UnreliableCar

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -7,3 +7,9 @@ from car import Car
 
 class UnreliableCar(Car):
     """Specialised version of a Car that includes reliability."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise a UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
