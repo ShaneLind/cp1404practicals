4fa11196aa4464b7e70a7c714aa889093a13283b
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 13:07:21 +1000

    add silver service taxi class and test files

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,8 @@
+"""
+CP1404 Practical
+SilverServiceTaxi class file
+"""
+
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+"""
+CP1404 Practical
+SilverServiceTaxi test file
+"""
+
+from silver_service_taxi import SilverServiceTaxi
