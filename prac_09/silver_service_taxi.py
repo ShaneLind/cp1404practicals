01a7a7c444ae0dbb5b77548b1b64bae52c11d449
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-16 13:29:30 +1000

    add ability to adjust price_per_km in regards to fanciness, add updated __str__ for new variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,4 +5,26 @@ SilverServiceTaxi class file
 
 from taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness: float):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.price_per_km = self.price_per_km * fanciness
+        self.current_fare_distance = 0
+        self.number_of_fares = 1
+
+    def get_fare(self):
+        """Return the price for the taxi trip like parent class,
+        but adds an extra charge for each now fare."""
+        current_fare = self.price_per_km * self.current_fare_distance + (self.flagfall * self.number_of_fares)
+        self.number_of_fares += 1
+        return current_fare
+
+    def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
+        return (f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km "
+                f"plus a flagfall of ${self.flagfall * self.number_of_fares}")
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,3 +4,18 @@ SilverServiceTaxi test file
 """
 
 from silver_service_taxi import SilverServiceTaxi
+
+my_taxi = SilverServiceTaxi(name="Hummer", fuel=200, fanciness=4)
+my_taxi2 = SilverServiceTaxi(name="Vehicle", fuel=200, fanciness=2)
+
+my_taxi.start_fare()
+my_taxi.drive(0)
+print(my_taxi.__str__())
+print(f"Current fare = ${my_taxi.get_fare()}")
+
+my_taxi2.start_fare()
+my_taxi2.drive(18)
+print(my_taxi2.__str__())
+print(f"Current fare = ${my_taxi2.get_fare()}")
+
+
