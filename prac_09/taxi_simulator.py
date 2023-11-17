645be09edd1470c49f08224c4cab2fe66d7f5c7b
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-17 12:06:38 +1000

    add choose taxi and drive functionality

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,17 +11,45 @@ MENU = """q)uit, c)hoose taxi, d)rive"""
 def main():
     """ ??????????    """
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-
+    current_taxi = None
+    bill = 0.00
+    print("Let's Drive!")
     print(MENU)
     choice = input(">>> ").upper()
 
     while choice != "Q":
         if choice == "C":
-            print("Option C")
-        elif choice == "Q":
-            print("Option Q")
-        else:
-            print('Invalid menu choice')
+            display_available_taxis(taxis)
+            taxi_choice = int(input("Choose Taxi:  "))
+            if 0 <= taxi_choice <= 2:
+                current_taxi = taxi_choice
+            else:
+                print("Invalid Taxi Choice")
 
+        elif choice == "D":
+
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                distance = float(input("Drive How Far? (km): "))
+                taxis[current_taxi].drive(distance)
+                bill += taxis[current_taxi].get_fare()
+                print(f"Your {taxis[current_taxi].name} trip cost you ${taxis[current_taxi].get_fare():.2f}")
+
+        else:
+            print("Invalid menu choice")
+
+        print(f"Bill to date: ${bill:.2f}")
         print(MENU)
         choice = input(">>> ").upper()
+
+
+def display_available_taxis(taxis):
+    print("Taxis Available:")
+    taxi_number = 0
+    for taxi in taxis:
+        print(f"{taxi_number} - {taxi.__str__()}")
+        taxi_number += 1
+
+
+main()
