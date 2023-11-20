fc6d27b5d90616c0ebc5687f306874b23eddb862
ShaneLind <shane.lind@my.jcu.edu.au>
2023-11-20 11:07:44 +1000

    Add repeat_string funtion now passes failing test

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -32,7 +32,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # 1. fix the repeat_string function above so that it passes the failing test
+    # TODO: 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -41,7 +41,7 @@ def run_tests():
     test_car = Car()
     assert test_car._odometer == 0, "Car does not set odometer correctly"
 
-    # 2. write assert statements to show if Car sets the fuel correctly
+    # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
@@ -50,14 +50,14 @@ def run_tests():
 
 run_tests()
 
-# 3. Uncomment the following line and run the doctests
+# TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
 # doctest.testmod()
 
-# 4. Fix the failing is_long_word function
+# TODO: 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
 
-# 5. Write and test a function to format a phrase as a sentence,
+# TODO: 5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
